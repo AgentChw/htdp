425042782892868276d5dea5f10f658409d9b7b1
Le Seignuer <tulkfx00427@funix.edu.vn>
2022-12-15 17:55:49 +0700

    chap08 exercises finished

---
+++ chap08/sp131.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex34) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+; A alon (a list of names) is :
+; -- '()
+; -- (cons String list-of-names)
+; represents a list of contact on a cell phone
+
+; List-of-names -> Boolean
+; determines whether "Flatt" is on a-list-of-names
+(define (contains-flatt? alon)
+  (cond
+    [(empty? alon) #false]
+    [(cons? alon) 
+     (or (string=? (first alon) "Flatt")
+        (contains-flatt? (rest alon)))]))
+
+; ex:
+(check-expect (contains-flatt? '()) #false)
+(check-expect (contains-flatt? (cons "Find" '())) #false)
+(check-expect (contains-flatt? (cons "Flatt" '())) #true)
+(check-expect 
+  (contains-flatt? 
+    (cons "A" (cons "Flatt" (cons "C" '()))))
+  #true)
+
+
